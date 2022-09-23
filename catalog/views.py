1354ac026aac3bc7a9ff3fab854510f91b03c466
Antony Nyagah <a.nyagah@huqas.org>
2022-09-23 17:56:09 +0300

    feat: used generic class-based list and detail views to create pages to view books
    #TODO finish the challenge to update the authors pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views import generic
 
 from .models import Author, Book, BookInstance, Genre
 
@@ -7,7 +8,7 @@ def index(request):
     """View function for home page of site."""
 
     # All books
-    all_books = Book.objects.all().order_by("title")
+    # all_books = Book.objects.all().order_by("title")
 
     # Generate counts of some of the main objects
     num_books = Book.objects.count()
@@ -23,7 +24,7 @@ def index(request):
     num_genres = Genre.objects.count()
 
     context = {
-        "all_books": all_books,
+        # "all_books": all_books,
         "num_books": num_books,
         "num_instances": num_instances,
         "num_instances_available": num_instances_available,
@@ -33,3 +34,15 @@ def index(request):
 
     # render the HTML template index.html with the data in the context variable
     return render(request, "index.html", context=context)
+
+
+class BookListView(generic.ListView):
+    model = Book
+    context_object_name = "book_list"  # your own name for the list as a template variable
+    # queryset = Book.objects.filter(title__icontains="war")[:5] # Get 5 books containing the title war
+    template_name = "books/book_list.html"
+    paginate_by = 5
+
+
+class BookDetailView(generic.DetailView):
+    model = Book
