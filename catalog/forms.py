02122b8c0789ca2ec4b5b7d2c1f22372c5620da5
Antony Nyagah <a.nyagah@huqas.org>
2022-09-29 11:38:49 +0300

    feat(catalog/forms.py,catalog/views.py): replaced the renewal form with the built-in `ModelForm` implementation

--- catalog/forms.py
+++ catalog/forms.py
@@ -1,13 +1,16 @@
 import datetime
 
 from django import forms
-
 from django.core.exceptions import ValidationError
+from django.forms import ModelForm
 from django.utils.translation import gettext_lazy as _
+
+from .models import BookInstance
 
 
 class RenewBookForm(forms.Form):
     """Form for renewing a loaned book's due date."""
+
     renewal_date = forms.DateField(
         help_text="Enter a date between now and 4 weeks (default 3)."
     )
@@ -25,3 +28,27 @@ class RenewBookForm(forms.Form):
 
         # Return the cleaned data
         return data
+
+
+class RenewBookModelForm(ModelForm):
+    def clean_due_back(self):
+        data: datetime.datetime = self.cleaned_data["due_back"]
+
+        # Check if a date is not in the past.
+        if data < datetime.date.today():
+            raise ValidationError(_("Invalid date - renewal in past"))
+
+        # Check if a date is in the allowed range (+4 weeks from today)
+        if data > datetime.date.today() + datetime.timedelta(weeks=4):
+            raise ValidationError(_("Invalid date - renewal more than 4 weeks ahead"))
+
+        # Return the cleaned data
+        return data
+
+    class Meta:
+        model = BookInstance
+        fields = ["due_back"]
+        labels = {"due_back": _("New renewal date")}
+        help_texts = {
+            "due_back": _("Enter a date between now and 4 weeks (default 3).")
+        }
--- catalog/views.py
+++ catalog/views.py
@@ -7,7 +7,7 @@ from django.shortcuts import get_object_or_404, render
 from django.urls import reverse
 from django.views import generic
 
-from .forms import RenewBookForm
+from .forms import RenewBookModelForm
 from .models import Author, Book, BookInstance, Genre
 
 
@@ -105,23 +105,23 @@ def renew_book_librarian(request, pk: int):
     # If this is a POST request the process the Form data
     if request.method == "POST":
         # Create a form instance and populate it with data from the request (binding)
-        form = RenewBookForm(request.POST)
+        form = RenewBookModelForm(request.POST)
         # Check if the form is valid
         if form.is_valid():
             # process the data in form.cleaned_data as required (here we just write it to the model due_back field)
-            book_instance.due_back = form.cleaned_data["renewal_date"]
+            book_instance.due_back = form.cleaned_data["due_back"]
             book_instance.save()
 
             # redirect to a new URL
             return HttpResponseRedirect(reverse("all-borrowed-books"))
         else:
             proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-            form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
+            form = RenewBookModelForm(initial={"due_back": proposed_renewal_date})
 
     # If this is a GET (or any other method) create the default form.
     else:
         proposed_renewal_date = datetime.date.today() + datetime.timedelta(weeks=3)
-        form = RenewBookForm(initial={"renewal_date": proposed_renewal_date})
+        form = RenewBookModelForm(initial={"due_back": proposed_renewal_date})
 
         context = {"form": form, "book_instance": book_instance}
 
